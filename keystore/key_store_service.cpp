1b75929a58c32882d21d04ee1df2a60a199923bd
Brian Young <bcyoung@google.com>
2018-01-29 23:57:29 +0000

    Revert "Add "Unlocked device required" parameter to keys"
    This reverts commit 5407bf1ece35ebc66c8c56e36a789b6fb89659d5.
    
    Reason for revert: Build breakages on elfin, gce_x86_phone.
    
    Bug: 72679761
    Bug: 67752510
    Change-Id: I1a4f8f725fe3aa7195f266ed77bbb7f4cccaa662

--- keystore/key_store_service.cpp
+++ keystore/key_store_service.cpp
@@ -364,7 +364,6 @@ Status KeyStoreService::lock(int32_t userId, int32_t* aidl_return) {
         return Status::ok();
     }
 
-    enforcement_policy.set_device_locked(true, userId);
     mKeyStore->lock(userId);
     *aidl_return = static_cast<int32_t>(ResponseCode::NO_ERROR);
     return Status::ok();
@@ -393,7 +392,6 @@ Status KeyStoreService::unlock(int32_t userId, const String16& pw, int32_t* aidl
         return Status::ok();
     }
 
-    enforcement_policy.set_device_locked(false, userId);
     const String8 password8(pw);
     // read master key, decrypt with password, initialize mMasterKey*.
     *aidl_return = static_cast<int32_t>(mKeyStore->readMasterKey(password8, userId));
@@ -1446,7 +1444,7 @@ Status KeyStoreService::isOperationAuthorized(const sp<IBinder>& token, bool* ai
 }
 
 Status KeyStoreService::addAuthToken(const ::std::vector<uint8_t>& authTokenAsVector,
-                                     int32_t android_uid, int32_t* aidl_return) {
+                                     int32_t* aidl_return) {
 
     // TODO(swillden): When gatekeeper and fingerprint are ready, this should be updated to
     // receive a HardwareAuthToken, rather than an opaque byte array.
@@ -1467,8 +1465,6 @@ Status KeyStoreService::addAuthToken(const ::std::vector<uint8_t>& authTokenAsVe
         *aidl_return = static_cast<int32_t>(KeyStoreServiceReturnCode(ErrorCode::INVALID_ARGUMENT));
         return Status::ok();
     }
-
-    enforcement_policy.set_device_locked(false, android_uid);
 
     mAuthTokenTable.AddAuthenticationToken(hidlVec2AuthToken(hidl_vec<uint8_t>(authTokenAsVector)));
     *aidl_return = static_cast<int32_t>(ResponseCode::NO_ERROR);
